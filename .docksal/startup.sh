b410132ff60f22c3736d7e23d4c61054954998d7
gcordner <geoffcordner@gmail.com>
2024-11-10 02:03:01 +0000

    Revert "more phpcs configuration"
    This reverts commit 0accb113f0bc3efc514b0fbb6c04aebfa52e31f1.

--- .docksal/startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Run composer install to install dependencies
-composer install
-
-# Set the installed paths for phpcs to recognize the WordPress standards
-vendor/bin/phpcs --config-set installed_paths /var/www/vendor/wp-coding-standards/wpcs
-
